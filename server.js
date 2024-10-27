4bc93fc0e1797d01cb4141765c204b98dd66edf0
Mohd iPhone <mohdiphone@Mohd-MacBook.local>
2024-10-27 15:51:38 +0530

    Forth Hosted verson

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/Menus',MenusRoutes);
 
 // Node Port
 
-const Port=process.env.Port
+const Port=process.env.Port || 2500
 app.listen(Port, () => {
     console.log(`Listening on port ${Port}`);
 });
